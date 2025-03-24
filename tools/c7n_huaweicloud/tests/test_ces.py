947bf7e935559965df351b3c200f9de4ad3e2581
1974365584@qq.com <Xuelinfei@qq.com>
2025-03-24 23:12:18 +0800

    UT commit

--- tools/c7n_huaweicloud/tests/test_ces.py
+++ tools/c7n_huaweicloud/tests/test_ces.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 from huaweicloud_common import BaseTest
 
+
 class AlarmTest(BaseTest):
 
     def test_alarm_query(self):
@@ -57,44 +58,74 @@ class AlarmTest(BaseTest):
         },
             session_factory=factory)
         resources = p.run()
-        self.assertEqual(resources[0]["alarm_id"], "al17427965140272BWJEvgrp")
-    #
-    # def test_create_kms_event_alarm_rule(self):
-    #     factory = self.replay_flight_data('create_kms_event_alarm_rule')
-    #     p = self.load_policy({
-    #         'name': 'alarm-kms-disable-or-delete-key',
-    #         'resource': 'huaweicloud.alarm'},
-    #         session_factory=factory)
-    #     resources = p.run()
-    #     self.assertEqual(len(resources), 1)
-    #     self.assertEqual(resources[0]['alarm_id'], "al17427965140272BWJEvgrp")
-    #
-    # def test_create_obs_event_alarm_rule(self):
-    #     factory = self.replay_flight_data('create_obs_event_alarm_rule')
-    #     p = self.load_policy({
-    #         'name': 'alarm-obs-bucket-policy-change',
-    #         'resource': 'huaweicloud.alarm'},
-    #         session_factory=factory)
-    #     resources = p.run()
-    #     self.assertEqual(len(resources), 1)
-    #     self.assertEqual(resources[0]['alarm_id'], "al17427965140272BWJEvgrp")
-    #
-    # def test_notif_by_smn(self):
-    #     factory = self.replay_flight_data('notif_by_smn')
-    #     p = self.load_policy({
-    #         'name': 'alarm-resource-check',
-    #         'resource': 'huaweicloud.alarm'},
-    #         session_factory=factory)
-    #     resources = p.run()
-    #     self.assertEqual(len(resources), 1)
-    #     self.assertEqual(resources[0]['alarm_id'], "al17427965140272BWJEvgrp")
-    #
-    # def test_create_vpc_event_alarm_rule(self):
-    #     factory = self.replay_flight_data('create_vpc_event_alarm_rule')
-    #     p = self.load_policy({
-    #         'name': 'alarm-vpc-change',
-    #         'resource': 'huaweicloud.alarm'},
-    #         session_factory=factory)
-    #     resources = p.run()
-    #     self.assertEqual(len(resources), 1)
-    #     self.assertEqual(resources[0]['alarm_id'], "al17427965140272BWJEvgrp")
+        self.assertEqual(len(resources), 0)
+
+    def test_create_kms_event_alarm_rule(self):
+        factory = self.replay_flight_data('create_kms_event_alarm_rule')
+        p = self.load_policy({
+            'name': 'alarm-kms-disable-or-delete-key',
+            'resource': 'huaweicloud.alarm',
+            'actions': [{
+                'type': 'create-kms-event-alarm-rule',
+                'parameters': {
+                    'action_type': 'notification',
+                    'notification_list': ['urn:smn:cn-north-4:test:test']
+                }
+            }]
+        },
+            session_factory=factory)
+        resources = p.run()
+        self.assertEqual(len(resources), 0)
+
+    def test_create_obs_event_alarm_rule(self):
+        factory = self.replay_flight_data('create_obs_event_alarm_rule')
+        p = self.load_policy({
+            'name': 'alarm-obs-bucket-policy-change',
+            'resource': 'huaweicloud.alarm',
+            'actions': [{
+                'type': 'create-obs-event-alarm-rule',
+                'parameters': {
+                    'action_type': 'notification',
+                    'notification_list': ['urn:smn:cn-north-4:test:test']
+                }
+            }]
+        },
+            session_factory=factory)
+        resources = p.run()
+        self.assertEqual(len(resources), 0)
+
+    def test_notif_by_smn(self):
+        factory = self.replay_flight_data('notif_by_smn')
+        p = self.load_policy({
+            'name': 'alarm-resource-check',
+            'resource': 'huaweicloud.alarm',
+            'actions': [{
+                'type': 'notify-by-smn',
+                'parameters': {
+                    'subject': "Test Subject",
+                    'message': "Test Message",
+                    'notification_list': ["urn:smn:cn-north-4:test:test"]
+                }
+            }]
+        },
+            session_factory=factory)
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertEqual(resources[0]['alarm_id'], "al17427965140272BWJEvgrp")
+
+    def test_create_vpc_event_alarm_rule(self):
+        factory = self.replay_flight_data('create_vpc_event_alarm_rule')
+        p = self.load_policy({
+            'name': 'alarm-vpc-change',
+            'resource': 'huaweicloud.alarm',
+            'actions': [{
+                'type': 'create-vpc-event-alarm-rule',
+                'parameters': {
+                    'action_type': 'notification',
+                    'notification_list': ['urn:smn:cn-north-4:test:test']
+                }
+            }]
+        },
+            session_factory=factory)
+        resources = p.run()
+        self.assertEqual(len(resources), 0)
